e5f494a7ced4197152b0b41e8fd639495d3ff9da
Oleh Pokotyliuk <olehshmdt@gmail.com>
2023-03-10 23:18:56 +0200

    added a adaptive and append visible class in js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
 
-    const accordeon = document.querySelectorAll('.accordeon')
+    const accordeon = document.querySelectorAll('.accordeon');
+    const allitems = document.querySelector('.allitems');
     const link = document.querySelectorAll('.link');
     const item = document.querySelectorAll('.accordeon-item');
     const img = document.querySelectorAll('.accordeon-item__img');
 
 
+
     item.forEach((e, arr) => {
         e.addEventListener('mouseenter', (event) => {
-            img[arr].classList.toggle('visible');
-            console.log(img[arr], arr);
+            img[arr].classList.toggle('visibleimg');
+
+            link.forEach(event => {
+                event.classList.toggle('visible');
+                event.innerHTML = '+';
+            })
+
         });
         e.addEventListener('mouseleave', (event) => {
-            img[arr].classList.toggle('hidden');
-            console.log(img[arr], arr);
+            img[arr].classList.toggle('hiddenimg');
+            link.forEach(event => {
+                event.classList.toggle('hidden');
+            })
         });
     });
 
