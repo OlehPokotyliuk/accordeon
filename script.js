4f500903961c189a028cc0e5b96178bb3dfa30d8
Oleh Pokotyliuk <olehshmdt@gmail.com>
2023-03-10 17:06:34 +0200

    fix: add next part in css, change hover in accordeon

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     const accordeon = document.querySelectorAll('.accordeon')
     const link = document.querySelectorAll('.link');
+    const item = document.querySelectorAll('.accordeon-item');
     const img = document.querySelectorAll('.accordeon-item__img');
 
 
-    link.forEach((e, arr) => {
+    item.forEach((e, arr) => {
         e.addEventListener('mouseenter', (event) => {
             img[arr].classList.toggle('visible');
             console.log(img[arr], arr);
         });
-    });
-    link.forEach((e, arr) => {
         e.addEventListener('mouseleave', (event) => {
             img[arr].classList.toggle('hidden');
             console.log(img[arr], arr);
         });
     });
-});
+
+})
