91355f25c616fd4c91e5d95c0cff2d51bac8621c
Oleh Pokotyliuk <olehshmdt@gmail.com>
2023-03-11 18:29:52 +0200

    fix: fixing classes in js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-
-    const accordeon = document.querySelectorAll('.accordeon');
-    const allitems = document.querySelector('.allitems');
-    const link = document.querySelectorAll('.link');
     const item = document.querySelectorAll('.accordeon-item');
     const img = document.querySelectorAll('.accordeon-item__img');
 
@@ -12,15 +8,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     item.forEach((e, arr) => {
         e.addEventListener('mouseenter', (event) => {
-            img[arr].classList.toggle('visibleimg');
+            img[arr].classList.add('visibleimg');
+        });
+        e.addEventListener('mouseleave', (event) => {
+            img[arr].classList.remove('visibleimg');
         });
     });
 
-    item.forEach((e, arr) => {
-        e.addEventListener('mouseleave', (event) => {
-            img[arr].classList.toggle('hiddenimg');
-        });
-    });
 
 
 })
